a464a269fb4a9b0b2ee460a111ef2538161c6987
saad <saadjsct@gmail.com>
2025-05-26 19:52:16 +0300

    update

---
+++ precache-manifest.941a85b68afb64b4b3d341bf7231dbf8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7ff4e1428af66651dac05f917599826",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68af3c42b08db37dba05",
+    "url": "/static/css/2.6883e14e.chunk.css"
+  },
+  {
+    "revision": "68af3c42b08db37dba05",
+    "url": "/static/js/2.477d89de.chunk.js"
+  },
+  {
+    "revision": "4c149b29b80552f0ce6397ecc8f9fb89",
+    "url": "/static/js/2.477d89de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9341ab7677c2f76b82c6",
+    "url": "/static/js/main.e3e402dc.chunk.js"
+  },
+  {
+    "revision": "9640c7c539e293f963b7",
+    "url": "/static/js/runtime-main.f6d5bed5.js"
+  }
+]);
